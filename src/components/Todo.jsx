0efbde1f2cbb7ed11fb081d3a463211490bd4be4
Harley Cardoso <harley.henrique@gmail.com>
2023-08-06 14:00:16 -0300

    Adicionado style css

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,10 +6,12 @@ const todo = ({todo,removeTodo,completeTodo}) => {
   return (
     <div className='todo' >   
         <div className="content" style={{textDecoration: todo.completo ? "line-through" : ""}}>
-        <p>{todo.texto}</p>
-        <p>({todo.categoria})</p>
-        <button onClick={()=>completeTodo(todo.id)}>Completar</button>
-        <button onClick={()=>removeTodo(todo.id)}>X</button>
+          <p>{todo.texto}</p>
+          <p className="category">({todo.categoria})</p>
+        </div>
+        <div>
+          <button className="complete" onClick={()=>completeTodo(todo.id)}>Completar</button>
+          <button className="remove" onClick={()=>removeTodo(todo.id)}>X</button>
         </div>
     </div>
   )
